a424f87af4010a077fddd389a6ba4450e5757fac
RohitKushvaha01 <oldisg131@gmail.com>
2025-06-15 15:50:38 +0530

    feat. copy on long press (Closes #7)

--- app/src/main/java/com/rk/components/TextCard.kt
+++ app/src/main/java/com/rk/components/TextCard.kt
@@ -1,9 +1,20 @@
 package com.rk.components
 
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
+import android.widget.Toast
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import com.rk.taskmanager.TaskManager
 
 @Composable
 fun TextCard(modifier: Modifier = Modifier,text: String,description: String? = null) {
-    SettingsToggle(label = text, description = description, default = false, showSwitch = false)
+    SettingsToggle(label = text, description = description, default = false, showSwitch = false, onLongClick = {
+        val clipboard = TaskManager.getContext().getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+        val clip = ClipData.newPlainText(text, description)
+        clipboard.setPrimaryClip(clip)
+
+        Toast.makeText(TaskManager.getContext(), "Copied", Toast.LENGTH_SHORT).show()
+    })
 }
